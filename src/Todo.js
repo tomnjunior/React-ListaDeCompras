5da4dd5d2c72cb7b4072c066b0ff5956ddf3d411
tomnjunior <tomnjunior@gmail.com>
2022-04-08 11:27:12 -0300

    React - Lista de Compras

--- src/Todo.js
+++ src/Todo.js
@@ -1,59 +1,66 @@
-import React, { useEffect, useState } from 'react'
-import './Todo.css'
-import List from './components/List'
-import TodoForm from './components/TodoForm'
-import Item from './components/Item'
+import React, { useEffect, useState } from "react";
+import "./Todo.css";
+import List from "./components/List";
+import TodoForm from "./components/TodoForm";
+import Item from "./components/Item";
+import Modal from "./components/Modal";
 
-const SAVED_ITEMS = "savedItems"
+const SAVED_ITEMS = "savedItems";
 
-function Todo(){
+function Todo() {
+  const [showModal, setShowModal] = useState(false);
+  const [items, setItems] = useState([]);
 
-    const[items, setItems] = useState([]);
-
-    useEffect(()=>{
-        let savedItems = JSON.parse(localStorage.getItem(SAVED_ITEMS))
-        if (savedItems){
-            setItems(savedItems);
-        }
-    },[])
-
-    useEffect(()=>{
-        localStorage.setItem(SAVED_ITEMS, JSON.stringify(items))
-    }, [items])
-
-    function onAddItem(text){
-
-        let it= new Item(text);
-
-        setItems([...items, it]);
+  useEffect(() => {
+    let savedItems = JSON.parse(localStorage.getItem(SAVED_ITEMS));
+    if (savedItems) {
+      setItems(savedItems);
     }
+  }, []);
 
-    function onItemDeleted(item){
-        let filteredItems = items.filter(it=>it.id != item.id);
-        setItems(filteredItems);
-    }
+  useEffect(() => {
+    localStorage.setItem(SAVED_ITEMS, JSON.stringify(items));
+  }, [items]);
 
-    function onDone(item){
-        let updatedItems = items.map(it=>{
-            if(it.id === item.id){
-                it.done = !it.done;
-            }
-            return it;
-        })
-        setItems(updatedItems);
-    }
+  function onAddItem(text) {
+    let it = new Item(text);
 
-    return (
+    setItems([...items, it]);
+    onHideModal();
+  }
+
+  function onItemDeleted(item) {
+    let filteredItems = items.filter((it) => it.id !== item.id);
+    setItems(filteredItems);
+  }
+
+  function onDone(item) {
+    let updatedItems = items.map((it) => {
+      if (it.id === item.id) {
+        it.done = !it.done;
+      }
+      return it;
+    });
+    setItems(updatedItems);
+  }
+
+  function onHideModal() {
+    setShowModal(false);
+  }
+
+  return (
     <div className="container">
-        <header className='header'><h1>Lista de Compras</h1><button className='addButton'> + </button></header>
-        
-        
+      <header className="header">
+        <h1>Lista de Compras</h1>
+        <button alt="Adicionar Item a Lista" title="Adicionar Item a Lista" onClick={() => {setShowModal(true);}} className="addButton"> + </button>
+      </header>
+      {/* <TodoForm onAddItem={onAddItem}></TodoForm> */}
+      <List onDone={onDone} onItemDeleted={onItemDeleted} items={items}></List>
+      <Modal show={showModal} onHideModal={onHideModal}>
         <TodoForm onAddItem={onAddItem}></TodoForm>
-        <List onDone={onDone} onItemDeleted={onItemDeleted} items={items}></List>
-
+      </Modal>
     </div>
-    )
+  );
 }
 
-
-export default Todo
+export default Todo;
